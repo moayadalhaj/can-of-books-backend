414f7332f9d46adc004b2c46b4a99d20e78ba8cd
moayadalhaj <moayad.alhaj21@gmail.com>
2021-08-17 01:02:34 +0300

    solve lab12

--- controllers/Book.controller.js
+++ controllers/Book.controller.js
@@ -4,8 +4,8 @@ const { BookSchema, SeedBooks } = require('../models/Book.model');
 const Book = mongoose.model('Books', BookSchema);
 
 const BookController = (req, res) => {
-  Book.find({}, (err, value) => {
-    res.send(value);
+  Book.find({}, (err, elements) => {
+    res.send(elements);
   })
 }
 
